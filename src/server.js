0544cc04f42c2c5d5524f12788c645b88e3d1140
JoaoLopesFilho <lopesjoaofilho@gmail.com>
2025-07-08 22:07:31 -0300

    gitignore

--- src/server.js
+++ src/server.js
@@ -1,17 +1,20 @@
 import express from "express";
+import cors from "cors";
 import filmesRoutes from "../src/routes/filmes.routes.js";
+
 const app = express();
 const PORT = 3000;
 
+app.use(cors()); 
 app.use(express.json());
 app.use('/filmes', filmesRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
-
+e
 app.get('/117', (req, res) => {
   res.status(200).json({ message: "Hello World" });
 });
 
-
+app.listen(PORT, () => {
+  console.log('Servidor rodando na porta ${PORT}');
+});
+Server.js
